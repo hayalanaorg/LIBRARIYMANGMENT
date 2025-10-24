52ade99892cbd070cfb39cea92a8046889dee939
abdathaya996-cpu <abdathaya996@gmail.com>
2025-10-24 19:25:42 +0300

    Admin test

--- src/main/java/library/Admin.java
+++ src/main/java/library/Admin.java
@@ -1,6 +1,27 @@
 package library;
-
 public class Admin {
+    private String username;
+    private String password;
+    private boolean loggedIn = false;
 
-	
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean login(String username, String password) {
+        if (this.username.equals(username) && this.password.equals(password)) {
+            loggedIn = true;
+            return true;
+        }
+        return false;
+    }
+
+    public void logout() {
+        loggedIn = false;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
 }
--- src/main/java/library/libraryy.java
+++ src/main/java/library/libraryy.java
@@ -1,5 +1,37 @@
 package library;
 
-public class libraryy {
+import java.util.ArrayList;
+import java.util.List;
 
+public class libraryy {
+    private List<Book> books; 
+
+    public libraryy() {
+        books = new ArrayList<>();
+    }
+
+    // 🔹 US1.3 – Add Book
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added: " + book.getTitle());
+    }
+
+    // 🔹 US1.4 – Search Book
+    public List<Book> searchBook(String keyword) {
+        List<Book> result = new ArrayList<>();
+
+        for (Book b : books) {
+            if (b.getTitle().toLowerCase().contains(keyword.toLowerCase()) ||
+                b.getAuthor().toLowerCase().contains(keyword.toLowerCase()) ||
+                b.getIsbn().equalsIgnoreCase(keyword)) {
+                result.add(b);
+            }
+        }
+        return result;
+    }
+
+    //
+    public List<Book> getAllBooks() {
+        return books;
+    }
 }
